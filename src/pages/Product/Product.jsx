d334392995f517ec564d66f50ea237240a56dfc1
Aryan Shrestha <aryanshrestha183@gmail.com>
2023-12-22 00:14:51 +0545

    faq section resolved

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -4,7 +4,7 @@ import { Link, useNavigate, useParams } from "react-router-dom";
 import { useFetch } from "../../hooks";
 import ProductImageViewer from "./components/ProductImageViewer";
 import { ShopCard } from "../../components";
-import { FAQSection } from "./components/FaqSection";
+import { FAQSection } from "./components/FAQSection";
 import { ReviewsSection } from "./components/ReviewsSection";
 
 const Product = () => {
